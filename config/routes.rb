bca966907446cc16f5696c1137f920906c818512
Zach <zachlandis91@gmail.com>
2023-06-28 12:34:54 -0600

    commit tree f7f2c95ef59e3860816084c5f43a99dee6649796

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
+  
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
+
+  
 end
